5c7f6f958649de95234873a1423b20ef5a1d40a0
panjiprann <panjipranantya20@gmail.com>
2024-03-19 15:31:59 +0700

    membuat input dan total harga

--- newelephant.cpp
+++ newelephant.cpp
@@ -2,8 +2,40 @@
 using namespace std;
 
 string nama;
-int bAdidas, bPuma, bNB, bcompass, bNike;
-int hAdidas = 300, hNB 
+int nAdidas, nPuma, nNB, nCompass, nNike;
+int hAdidas = 300, hNB = 250, hPuma = 150, hNike = 350;
+
+void input()
+{
+    cout << "masukkan jumlah Adidas = ";
+    cin >> nAdidas;
+
+    cout << "masukkan jumlah Puma = ";
+    cin >> nPuma;
+
+    cout << "masukkan jumlah New Balance = ";
+    cin >> nNB;
+
+    cout << "masukkan jumlah Compass = ";
+    cin >> nCompass;
+
+    cout << "masukkan jumlah Nike = ";
+    cin >> nNike;
+
+}
+
+int totalHarga()
+{
+    return (nAdidas * hAdidas) + (nPuma * hPuma) + (nNB * hNB) + (nNike * hNike);
+}
+
+  void display(){
+
+    cout << endl;
+    cout << "==================" << endl;
+    cout << endl;
+    cout << ""
+ }
 
 int main()
 {

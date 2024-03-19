126df46d6595b5bc1317f4f3ce37f8819de4cfd4
panjiprann <panjipranantya20@gmail.com>
2024-03-19 14:37:52 +0700

    smith

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i; string nama;
+    for (i = 7; i < 0; i--)
+    {
+        cout << "smith" << endl;
+    }
+
+    for (i = 0; i < 5; i++)
+    {
+        cout << "Masukkan nama =" ;
+        cin >> nama[i];
+
+    }
+
+    cout << endl;
+    cout << "===================" << endl;
+    cout << "KUMPULAN NAMA - NAMA" << endl;
+
+    for (i=0; i<5;i++) {
+        
+        cout << "namanya adalah " << nama[i] <<endl;
+    }
+}

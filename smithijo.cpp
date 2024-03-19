32725388457f135500be5391517acbd146f28955
panjiprann <panjipranantya20@gmail.com>
2024-03-19 14:58:52 +0700

    buat looping dengan while

---
+++ smithijo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+
+    int bilangan;
+
+    srand(time(0));
+
+    bilangan = rand() % 10;
+
+    while (bilangan%2)
+    {
+     cout << "bilangan " bilangan << "adalah ganjil";
+     bilangan = rand() % 10;
+    }
+     cout << "bilangan " bilangan << "adalah genap" << endl;
+
+}
+    
